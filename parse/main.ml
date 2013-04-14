e4b92d89459d39030570e2d112ce88c18ebc4a4b
David Scott <dave.scott@eu.citrix.com>
2013-04-14 11:32:54 +0100

    Print progress information while parsing the CSV data.

--- parse/main.ml
+++ parse/main.ml
@@ -2,7 +2,7 @@ open Core.Std
 open Async.Std
 
 let stdout_writer = Lazy.force Writer.stdout
-let log s = Writer.write stdout_writer s
+let log fmt = Printf.ksprintf (Writer.write stdout_writer) fmt
 
 (* based on the Command example: *)
 let uses_async : (unit -> int Deferred.t, unit -> unit) Command.Spec.t =
@@ -11,22 +11,26 @@ let uses_async : (unit -> int Deferred.t, unit -> unit) Command.Spec.t =
     let () = never_returns (Scheduler.go ()) in
     ())
 
+let spinner = [| '|'; '/'; '-'; '\\' |]
+
 let command =
   Command.basic ~summary:"parse the HYG star database"
     Command.Spec.(empty +> anon ("FILE" %: string) ++ uses_async)
     (fun path () ->
       Reader.with_file path ~f:(fun r ->
-        let rec loop () =
+        let rec loop sofar =
           Reader.read_line r
           >>= function
           | `Ok line ->
             let s = Star.of_string line in
-            let txt = match s with None -> "None" | Some s -> Star.to_string s in
-            Writer.write (Lazy.force Writer.stdout) (txt ^ "\n");
-            loop ()
+            let sofar = match s with None -> sofar | Some _ -> sofar + 1 in
+            if sofar mod 10000 = 0
+            then log "\r[%c] %10d" (spinner.( (sofar / 10000) mod (Array.length spinner))) sofar;
+            loop sofar
           | `Eof ->
+            log "\nRead a total of %d valid records.\n" sofar; 
             return () in
-        loop ()
+        loop 0
       )
       >>= fun () ->
       return 0
